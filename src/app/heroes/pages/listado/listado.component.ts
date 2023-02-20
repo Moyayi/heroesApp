4fb325141de6a828f48ace0a3e3a995de2ae0856
Pedro Moyano <pedro.moyano.garcia@gmail.com>
2023-02-20 19:39:40 +0100

    WIP show data result from a fetch

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit{
+
+  constructor( private heroesService : HeroesService) {}
+
+  heroes : Heroe[] = [];
+
+  ngOnInit(): void {
+    this.heroesService.getHeroes()
+      .subscribe( resp => {
+        this.heroes = resp
+      })
+  }
 
 }
